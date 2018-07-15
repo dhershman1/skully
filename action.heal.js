bd8f33913c8e677c37822dcf34e67756c09acdfc
dhershman1 <dustinh17@gmail.com>
2018-07-14 20:49:32 -0400

    tweak

--- action.heal.js
+++ action.heal.js
@@ -4,8 +4,8 @@ const heal = (creep, room) => {
   const target = _.filter(room.creeps, (creep) => {
     return creep.hits < 200
   })
-  if (target.length){
-    if (creep.heal(target[0]) === ERR_NOT_IN_RANGE){
+  if (target.length) {
+    if (creep.heal(target[0]) === ERR_NOT_IN_RANGE) {
       creep.moveTo(target[0])
     }
     return 'heal'
